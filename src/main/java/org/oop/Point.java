57db9756835597f57bfda1a2120a83b19c946a50
rakurs987@gmail.com <rakurs987@gmail.com>
2020-10-18 12:37:22 +0530

    Refactored code to oops

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -4,24 +4,21 @@ public class Point {
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
+
+    public static double distanceValue(Point p1, Point p2) {
+        double x_value = p2.x - p1.x;
+        double y_value = p2.y - p1.y;
+        return Math.sqrt(Math.pow(x_value, 2) + Math.pow(y_value, 2));
+    }
+
+    public static double directionValue(Point p1, Point p2) {
+        double x_value = p2.x - p1.x;
+        double y_value = p2.y - p1.y;
+        return Math.atan2(y_value, x_value);
+    }
+
 }
